47ef55d20d94d89511820fa798c7a2356f8e7d79
FillmoreBox <gcoleman@collectfanatics.com>
2026-07-16 14:12:52 -0700

    deploy v1784236372

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import { BRANDS, QUESTIONS } from "./data.js?v=1784236042";
-import { score, topMatches, wildcard, maxScore } from "./scoring.js?v=1784236042";
+import { BRANDS, QUESTIONS } from "./data.js?v=1784236372";
+import { score, topMatches, wildcard, maxScore } from "./scoring.js?v=1784236372";
 
 // One tally submission per page load, fire-and-forget; never blocks the reveal.
 let submitted = false;
